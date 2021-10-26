0e2532957a09b044000e58ed174ddf38d27f53fc
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-10-26 22:01:10 +0900

    条件分岐について

--- test.rb
+++ test.rb
@@ -1,15 +1,25 @@
-name = "tom"
-puts name
-number = 100
-puts number
+# name = "tom"
+# puts name
+# number = 100
+# puts number
 
-puts "私の好きな車はベンツ"
-puts "車といったらベンツ"
+# puts "私の好きな車はベンツ"
+# puts "車といったらベンツ"
 
-car = "ベンツ"
+# car = "ベンツ"
 
-puts "私の好きな車は" + car
-puts "車といったら" + car
+# puts "私の好きな車は" + car
+# puts "車といったら" + car
 
-age = 30
-puts "#{age}歳になります"
+# age = 30
+# puts "#{age}歳になります"
+
+# age = 18
+# if age < 20
+#   puts "あなたは未成年です"
+# end
+
+age = 22
+if age < 20
+  puts "あなたは未成年です"
+end
