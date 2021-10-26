19a44cac9320d790dcbc7de004a86a2299a2274a
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-10-26 21:52:55 +0900

    Ruby基礎知識の復習

--- test.rb
+++ test.rb
@@ -1,1 +1,15 @@
-puts "Hello World"
+name = "tom"
+puts name
+number = 100
+puts number
+
+puts "私の好きな車はベンツ"
+puts "車といったらベンツ"
+
+car = "ベンツ"
+
+puts "私の好きな車は" + car
+puts "車といったら" + car
+
+age = 30
+puts "#{age}歳になります"
