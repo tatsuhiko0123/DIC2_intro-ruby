51166a9bcc3de06e840721b97b6650d199b9093c
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-11-02 16:00:23 +0900

    引数について

--- task.rb
+++ task.rb
@@ -1,7 +1,46 @@
-def weather
-  puts "晴れ"
-  puts "曇り"
-  puts "雨"
-end
+# def weather
+#   puts "晴れ"
+#   puts "曇り"
+#   puts "雨"
+# end
 
-weather
+# weather
+
+# def play(sports)
+#   puts "私は今日#{sports}をします"
+# end
+
+# play("野球")
+
+# def add(n, y)
+#   puts n + y
+# end
+# add(2, 3)
+
+# puts "私は野球をします"
+# puts "私はサッカーをします"
+# puts "私がバスケットをします"
+
+# def play(sports)
+#   puts "私は#{sports}をします"
+# end
+
+# play("野球")
+# play("サッカー")
+# play("バスケット")
+
+# def introduce(name, age)
+#   puts "私の名前は#{name}です。#{age}歳です。"
+# end
+# puts "名前を入力して下さい"
+# name = gets.chomp
+# puts "年齢を入力してください"
+# age = gets.to_i
+# introduce(name, age)
+
+def introduce(name, age)
+  puts "私の名前は#{name}です。#{age}歳です。"
+end
+name = "toyoshima"
+age = 38
+introduce(name, age)
