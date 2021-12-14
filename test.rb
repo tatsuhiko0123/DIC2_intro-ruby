9f6d6ef18b90ea7ee6a8808aab3de7a91025e9d1
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-12-14 19:48:09 +0900

    アルゴリズム演習

--- test.rb
+++ test.rb
@@ -142,54 +142,63 @@
 #   puts "#{key} #{value}" 
 # end
 
-class School
-  def name=(word)
-    @school_name = word
+# class School
+#   def name=(word)
+#     @school_name = word
+#   end
+#   def name1
+#     puts @school_name
+#   end
+# end
+# school = School.new
+# school.name = "A学校"
+# school.name1
+
+# class School
+#   attr_accessor :name, :address
+#   def initialize(name, address)
+#     @name = name
+#     @address = address
+#   end
+# end
+# a_school = School.new("A学校", "渋谷区")
+# puts a_school.name
+# puts a_school.address
+# puts a_school.instance_variables
+
+
+# class School
+#   attr_accessor :name,
+#                 :address,
+#                 :number_of_students,
+#                 :founding_years,
+#                 :introduction_video_url,
+#                 :introduction_statement
+#   def initialize(name, address, number_of_students,founding_years,
+#                  introduction_video_url, introduction_statement)
+#     @name = name
+#     @address = address
+#     @number_of_students = number_of_students
+#     @founding_years = founding_years
+#     @introduction_video_url = introduction_video_url
+#     @introduction_statement = introduction_statement
+#   end
+# end
+
+# a_school = School.new("A学校", "東京都渋谷区..", 300, 100, "https://hoge.com", "A学校は自然豊かな...")
+# puts a_school.name
+# puts a_school.address
+# puts a_school.number_of_students
+# puts a_school.founding_years
+# puts a_school.introduction_video_url
+# puts a_school.introduction_statement
+# puts a_school.instance_variables
+
+def count_letter(str)
+  count = 0
+  str.split("").each do
+    count += 1
   end
-  def name1
-    puts @school_name
-  end
+  count
 end
-school = School.new
-school.name = "A学校"
-school.name1
-
-class School
-  attr_accessor :name, :address
-  def initialize(name, address)
-    @name = name
-    @address = address
-  end
-end
-a_school = School.new("A学校", "渋谷区")
-puts a_school.name
-puts a_school.address
-puts a_school.instance_variables
-
-
-class School
-  attr_accessor :name,
-                :address,
-                :number_of_students,
-                :founding_years,
-                :introduction_video_url,
-                :introduction_statement
-  def initialize(name, address, number_of_students,founding_years,
-                 introduction_video_url, introduction_statement)
-    @name = name
-    @address = address
-    @number_of_students = number_of_students
-    @founding_years = founding_years
-    @introduction_video_url = introduction_video_url
-    @introduction_statement = introduction_statement
-  end
-end
-
-a_school = School.new("A学校", "東京都渋谷区..", 300, 100, "https://hoge.com", "A学校は自然豊かな...")
-puts a_school.name
-puts a_school.address
-puts a_school.number_of_students
-puts a_school.founding_years
-puts a_school.introduction_video_url
-puts a_school.introduction_statement
-puts a_school.instance_variables
+p count_letter("abcde")
